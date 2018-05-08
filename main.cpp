114d67f71519964f3d080778f944957f29d7c8ae
mi tsu <hachimitsu1119@gmail.com>
2018-05-09 00:01:34 +0900

    26進数の表記をab...yzから01...opへ変更

--- main.cpp
+++ main.cpp
@@ -7,7 +7,11 @@ long long Conv(string s) {
 	int num;
 	long long ans = 0;
 	for(int i=0; i<(int)s.size(); i++) {
-		num = (int)s[i]-96;	//i文字目のアルファベットの数値 a:1 b:2 ...
+		if((int)s[i] > 96)	{	//アルファベットのとき
+			num = (int)s[i]-87;	//i文字目のアルファベットの数値 a:10 b:11 ...
+		} else {										//suuchi
+			num = (int)s[i]-48;	//i文字目の数字の数値 0:0 1:1 ...
+		}
 		ans += num*pow(26,(int)s.size()-1-i);
 	}
 	return ans;
