ea84685ed380531ad133873406171adc13b027ca
mi tsu <hachimitsu1119@gmail.com>
2018-05-10 15:21:41 +0900

    62進数まで対応

--- main.cpp
+++ main.cpp
@@ -3,25 +3,32 @@
 #include <math.h>
 using namespace std;
 
-long long Conv(string s) {
+long long Conv(int decimal, string s) {
 	int num;
 	long long ans = 0;
-	for(int i=0; i<(int)s.size(); i++) {
-		if((int)s[i] > 96)	{	//アルファベットのとき
-			num = (int)s[i]-87;	//i文字目のアルファベットの数値 a:10 b:11 ...
-		} else {										//数値のとき
-			num = (int)s[i]-48;	//i文字目の数字の数値 0:0 1:1 ...
+	int string_size = (int)s.size();
+	for(int i=0; i<string_size; i++) {
+		if((int)s[i] > 96)	{							//小文字ののとき
+			num = (int)s[i]-87;						//i文字目のアルファベットの数値 a:10 b:11 ...
+		} else if((int)s[i] > 64) {			//大文字のとき
+			num = (int)s[i]-29;						//i文字目のアルファベットの数値 A:35 B:36 ...
+		} else {															//数値のとき
+			num = (int)s[i]-48;						//i文字目の数字の数値 0:0 1:1 ...
 		}
-		ans += num*pow(26,(int)s.size()-1-i);
+		ans += num*pow(decimal,string_size-1-i);
 	}
 	return ans;
 }
 
 int main() {
 	string s;
+	int decimal;
+	cout << "何進数を10進数へ変換しますか(2-62)：";
+	cin >> decimal;
+	cout << "変換したい数値(0,1,2,...a,b,c,...A,B,C...Z)" << endl;
 	cin >> s;
 	
-	cout << Conv(s) << endl;
+	cout << Conv(decimal,s) << endl;
 	
 	return 0;
 }
